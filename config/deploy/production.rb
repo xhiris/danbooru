4df884e9990966829e931cca9461f8b1c160f10a
xhiris <xhiris@gmail.com>
2015-11-04 16:54:45 -0800

    Try production instead
    Fix implied by https://github.com/r888888888/danbooru/issues/2515 (I
    have no idea what I'm doing, of course)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
-set :user, "albert"
+set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "localhost", :roles => %w(web app db), :primary => true
+#~ set :user, "albert"
+#~ set :rails_env, "production"
+#~ server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
+#~ server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
